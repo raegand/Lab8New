37bb8083d52b793c4cc7ba3d93da22788cecdafd
felixjos <felixjos@hawaii.edu>
2014-04-17 17:02:01 -1000

    Initial commit from local git

---
+++ EE367_Lab8/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  EE367_Lab8
+//
+//  Created by Joseph Felix on 4/17/14.
+//  Copyright (c) 2014 Joseph Phillip Felix Jr. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
